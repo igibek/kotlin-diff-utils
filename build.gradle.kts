f99eb3e7ec12c1f39a2d04ceaf0f9932f4eecea9
Andrew Reed <andrew_reed@hotmail.com>
2021-10-29 12:24:38 +0100

    changed name replacement

--- build.gradle.kts
+++ build.gradle.kts
@@ -110,7 +110,7 @@ tasks {
         from.writeText(
             from.readText()
                 .replace("https://registry.npmjs.org/","https://npm.pkg.github.com/")
-                .replace("\"name\": \"kotlin-diff-utils\",", "\"name\": \"@gitliveapp/kotlin-diff-utils\",")
+                .replace("\"name\": \"kotlin-diff-utils-js-legacy\",", "\"name\": \"@gitliveapp/kotlin-diff-utils-js-legacy\",")
         )
     }
 }
